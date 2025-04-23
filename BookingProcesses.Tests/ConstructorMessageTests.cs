304efdc24946a8d9b49c878116ca839d08cf34d8
Vitaly <vitaly@example.com>
2025-04-23 17:31:55 +0300

    добавлены два теста для тестирования поля и свойства класса constructormessage

---
+++ BookingProcesses.Tests/ConstructorMessageTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace BookingProcesses.Tests
+{
+    [TestFixture]
+    public class ConstructorMessageTests
+    {
+        [Test]
+        public void ConstructoredMessage_ByDefault_ReturnsNull()
+        {
+            ConstructorMessage constructorMessage = new BookingProcesses.ConstructorMessage();
+            var result = constructorMessage.ConstructoredMessage;
+           Assert.That(result,Is.Empty);          
+        }
+        [Test]
+        public void ConstructoredMessage_NewValue_ReturnsNotNull()
+        {
+            ConstructorMessage constructorMessage = new BookingProcesses.ConstructorMessage();
+            String newValue = "NewValue";
+            constructorMessage.ConstructoredMessage = newValue;
+            var result = constructorMessage.ConstructoredMessage;
+            Assert.That(result, !Is.Null);
+        }
+    }
+}
---
+++ BookingProcesses/ConstructorMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace BookingProcesses
+{
+    public class ConstructorMessage
+    {
+        private String _constructoredMessage = String.Empty;
+        public String ConstructoredMessage
+        {
+            get { return _constructoredMessage; }
+            set { _constructoredMessage = value; }
+        }
+
+
+    }
+}
