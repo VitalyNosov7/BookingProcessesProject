f38f680df0e78d988beb57ed3f2150b97dedc05d
German <german@german.com>
2025-04-23 22:22:04 +0300

    Добавлены два теста, которые тестируют поле и свойство шаблона сообщений _templateMessage и TemplateMessage

--- BookingProcesses.Tests/ConstructorMessageTests.cs
+++ BookingProcesses.Tests/ConstructorMessageTests.cs
@@ -5,20 +5,44 @@ namespace BookingProcesses.Tests
     [TestFixture]
     public class ConstructorMessageTests
     {
+        private ConstructorMessage MakeConstructorMessage()
+        {
+            return new ConstructorMessage();
+        }
+
         [Test]
         public void ConstructoredMessage_ByDefault_ReturnsNull()
         {
-            ConstructorMessage constructorMessage = new BookingProcesses.ConstructorMessage();
+            ConstructorMessage constructorMessage = MakeConstructorMessage();
             var result = constructorMessage.ConstructoredMessage;
            Assert.That(result,Is.Empty);          
         }
+
         [Test]
-        public void ConstructoredMessage_NewValue_ReturnsNotNull()
+        public void ConstructoredMessage_NewMessage_ReturnsNotNull()
         {
-            ConstructorMessage constructorMessage = new BookingProcesses.ConstructorMessage();
+            ConstructorMessage constructorMessage = MakeConstructorMessage();
             String newValue = "NewValue";
             constructorMessage.ConstructoredMessage = newValue;
             var result = constructorMessage.ConstructoredMessage;
+            Assert.That(result, !Is.Null);
+        }
+
+        [Test]
+        public void TemplateMessage_ByDefault_ReturnNull()
+        {
+            ConstructorMessage constructorMessage = MakeConstructorMessage();
+            var result = constructorMessage.TemplateMessage;
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void TemplateMessage_NewTemplate_ReturnsNotNull()
+        {
+            ConstructorMessage constructorMessage = MakeConstructorMessage();
+            String newTemplate = "NewTemplate";
+            constructorMessage.TemplateMessage = newTemplate;
+            var result = constructorMessage.TemplateMessage;
             Assert.That(result, !Is.Null);
         }
     }
--- BookingProcesses/ConstructorMessage.cs
+++ BookingProcesses/ConstructorMessage.cs
@@ -1,14 +1,21 @@
-﻿namespace BookingProcesses
+﻿
+
+namespace BookingProcesses
 {
     public class ConstructorMessage
     {
-        private String _constructoredMessage = String.Empty;
+        private String _constructoredMessage = String.Empty;        
         public String ConstructoredMessage
         {
             get { return _constructoredMessage; }
             set { _constructoredMessage = value; }
         }
 
-
+        private String _templateMessage = String.Empty;
+        public String TemplateMessage
+        {
+            get { return _templateMessage; }
+            set { _templateMessage = value; }
+        }
     }
 }
