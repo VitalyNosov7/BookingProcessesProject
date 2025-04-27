e04922f24af3448fb23f6eed4e072006b0dfff90
German <german@german.com>
2025-04-27 15:04:36 +0300

    Добавлен классы Sanatorium  и SanatoriumTests

---
+++ BookingProcesses.Tests/SanatoriumTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace BookingProcesses.Tests
+{
+    [TestFixture]
+    public class SanatoriumTests
+    {
+        private Sanatorium MakeSanatorium()
+        {
+            return new Sanatorium();
+        }
+
+        /// <summary>Тест значения поля по умолчанию(через свойство)</summary>
+        [Test]
+        public void SanatoriumName_ByDefaut_ReturnsNull ()
+        {
+            Sanatorium sanatorium = MakeSanatorium();
+            var result = sanatorium.SanatoriumName;
+            Assert.That (result, Is.Null);
+        }
+
+        /// <summary>Тест записи-чтения поля(через свойство)</summary>
+        [Test]
+        public void Sanatorium_NewSanatoriumName_ResultsNotNull ()
+        {
+            Sanatorium sanatorium =  MakeSanatorium();
+            String newSanatoriumName = "New Sanatorium Name";
+            var result = sanatorium.SanatoriumName == newSanatoriumName;
+            Assert.That(result, !Is.Null);
+        }
+    }
+}
---
+++ BookingProcesses/Sanatorium.cs
@@ -0,0 +1,18 @@
+﻿namespace BookingProcesses
+{
+    /// <summary>
+    /// Санаторий.
+    /// В этом классе хранится информация о санатории.
+    /// </summary>
+    public class Sanatorium
+    {
+        /// <summary>Название санатория.</summary>
+        private String? _sanatoriumName = default;
+        /// <summary>Название санатория.</summary>
+        public String SanatoriumName
+        {
+            get { return _sanatoriumName!; }
+            set { _sanatoriumName = value; }
+        }
+    }
+}
