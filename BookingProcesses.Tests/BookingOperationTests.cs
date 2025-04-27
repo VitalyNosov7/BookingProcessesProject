6b5bcd6f16db3992925e688a4e0017a7b871c55d
German <german@german.com>
2025-04-27 22:57:03 +0300

    В классе BookingOperation добавлено поле и свойство типа Booking, конструктор. Добавлен тест для этих поля и свойства.

--- BookingProcesses.Tests/BookingOperationTests.cs
+++ BookingProcesses.Tests/BookingOperationTests.cs
@@ -7,7 +7,7 @@ namespace BookingProcesses.Tests
     {
         private BookingOperation MakeBookingOperation()
         {
-            return new BookingOperation();
+            return new BookingOperation(null);
         }
 
         /// <summary>Тест значения поля по умолчанию(через свойство)</summary>
@@ -27,6 +27,26 @@ namespace BookingProcesses.Tests
             String newOperationName = "New Operation Name";
             currentBookingOperation.OperationName = newOperationName;
             var result = currentBookingOperation.OperationName;
+            Assert.That(result, !Is.Null);
+        }
+
+        /// <summary>Тест значения поля по умолчанию(через свойство)</summary>
+        [Test]
+        public void CurrentBooking_ByDefault_ReturnsNull()
+        {
+            BookingOperation currentBookingOperation = MakeBookingOperation();
+            var result = currentBookingOperation.CurrentBooking;
+            Assert.That(result, Is.Null);
+        }
+
+        /// <summary>Тест записи-чтения поля(через свойство)</summary>
+        [Test]
+        public void CurrentBooking_NewCurrentBooking_ReturnsNotNull()
+        {
+            BookingOperation currentBookingOperation = MakeBookingOperation();
+            Booking newCurrentBooking = new Booking();
+            currentBookingOperation.CurrentBooking = newCurrentBooking;
+            var result = currentBookingOperation.CurrentBooking;
             Assert.That(result, !Is.Null);
         }
     }
--- BookingProcesses/BookingOperation.cs
+++ BookingProcesses/BookingOperation.cs
@@ -2,13 +2,19 @@
 {
     /// <summary>
     /// Операция бронирования.
-    /// В этом классе хранятся данные определенной операции бронирования.
+    /// В этом классе хранятся данные и методы определенной операции бронирования.
     /// </summary>
     /*
      Например: формирование счета, формирование заявки, аннуляция брони, коррекция заявки и т.п.
      */
     public class BookingOperation
     {
+        public BookingOperation(Booking? booking)
+        {
+            CurrentBooking = booking!;
+        }
+
+        #region СВОЙСТВА
         /// <summary>Название операции бронирования.</summary>
         private String? _operationName = default;
         /// <summary>Название операции бронирования.</summary>
@@ -17,5 +23,16 @@
             get { return _operationName!; }
             set { _operationName = value; } 
         }
+
+        /// <summary>Текущая бронь.</summary>
+        private Booking? _currentBooking = default;
+        /// <summary>Текущая бронь.</summary>
+        public Booking CurrentBooking
+        {
+            get { return _currentBooking!; }
+            set { _currentBooking = value; }
+        }
+        #endregion
+
     }
 }
